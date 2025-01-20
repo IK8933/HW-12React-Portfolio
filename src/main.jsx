1b1a720feef8153ca929fe81d61b4ca9d4f0de95
IK8933 <iankessack1989@gmail.com>
2025-01-20 15:08:43 -0500

    updated2

--- src/main.jsx
+++ src/main.jsx
@@ -1,58 +1,14 @@
-// import ReactDOM from 'react-dom/client';
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import App from './App.jsx';
-// import Error from './pages/Error';
-// import Home from './pages/Home';
-// import Contact from './pages/Contact';
-// import Aboutme from './Pages/Aboutme';
-// import Resume from './Pages/Resume';
-// import Portfolio from './Pages/Portfolio';
-// import './index.css';
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     errorElement: <Error />, 
-//     children: [
-//       {
-//         index: true,
-//         element: <Home />,
-//       },
-//       {
-//         path: '/About',
-//         element: <Aboutme />,
-//       },
-//       {
-//         path: '/Portfolio',
-//         element: <Portfolio />,
-//       },
-//       {
-//         path: '/Contact',
-//         element: <Contact />,
-//       },
-//       {
-//         path: '/Resume',
-//         element: <Resume />,
-//       },
-//     ],
-//   },
-// ]);
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <RouterProvider router={router} />
-// );
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import Error from './pages/Error';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import Aboutme from './pages/Aboutme'; // Fixed case
-import Resume from './pages/Resume';   // Fixed case
-import Portfolio from './pages/Portfolio'; // Fixed case
+import Aboutme from './Pages/Aboutme';
+import Resume from './Pages/Resume';
+import Portfolio from './Pages/Portfolio';
 import './index.css';
+
 
 const router = createBrowserRouter([
   {
