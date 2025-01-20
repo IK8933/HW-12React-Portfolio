2eecb0758e9f438565c14dc0f7ff46bac4e60e56
IK8933 <iankessack1989@gmail.com>
2025-01-20 15:37:17 -0500

    resume

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,24 +1,3 @@
-// import resumePDF from '../assets/IKessack_CV 2024.pdf';
-// const Resume = () => {
-//     return (
-//         <section className="p-8 bg-gray-600 text-center">
-//             <h1 className="text-3xl font-bold mb-4">My Resume</h1>
-//             <p className="mb-4 dark:text-blue-500 text-blue-500">Click the button below to view or download my resume.</p>
-//             <a
-//                 href={resumePDF} 
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//                 className="inline-block bg-blue-500 hover:bg-blue-700 hover:text-white text-white font-bold py-2 px-4 rounded"
-//             >
-//                 View My Resume
-//             </a>
-//         </section>
-//     );
-// };
-
-// export default Resume;
-
-
 import resumePDF from '../assets/IKessack_CV 2024.pdf';
 
 const Resume = () => {
