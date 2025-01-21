de2c88fa7643df2ba4db02110b85e3e1ff0cf517
IK8933 <iankessack1989@gmail.com>
2025-01-21 16:36:09 -0500

    code4

--- src/temp/Home.jsx
+++ src/temp/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <h2 className="text-2xl text-blue-900 dark:text-blue-400 font-bold mb-4">Let’s Collaborate!</h2>
     <p className="text-lg mb-6 text-gray-800 dark:text-gray-100">Have a project in mind or just want to connect? Feel free to reach out!</p>
     <a
-        href="/contact"
+        href="/Contact"
         className="bg-blue-500 hover:bg-blue-700 hover:text-gray-300 text-white py-2 px-4 rounded text-lg"
     >
         Contact Me
