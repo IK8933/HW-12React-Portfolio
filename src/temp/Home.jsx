3b3164fd2456f71aac418fc9dcdb9fa4428e5c2c
IK8933 <iankessack1989@gmail.com>
2025-01-22 15:05:19 -0500

    updated home

--- src/temp/Home.jsx
+++ src/temp/Home.jsx
@@ -1,4 +1,6 @@
 import HeroSection from './HeroSection';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
     return (
         
@@ -7,12 +9,12 @@ const Home = () => {
                 <section className="mt-8 text-center py-8 bg-transparent dark:bg-transparent">
     <h2 className="text-2xl text-blue-900 dark:text-blue-400 font-bold mb-4">Let’s Collaborate!</h2>
     <p className="text-lg mb-6 text-gray-800 dark:text-gray-100">Have a project in mind or just want to connect? Feel free to reach out!</p>
-    <a
-        href="/Contact"
-        className="bg-blue-500 hover:bg-blue-700 hover:text-gray-300 text-white py-2 px-4 rounded text-lg"
-    >
-        Contact Me
-    </a>
+    <Link
+                    to="/contact"
+                    className="bg-blue-500 hover:bg-blue-700 hover:text-gray-300 text-white py-2 px-4 rounded text-lg"
+                >
+                    Contact Me
+                </Link>
 </section>
         </div>
     );
